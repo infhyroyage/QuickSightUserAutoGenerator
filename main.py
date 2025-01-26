678ca8718555ecef0839fb7da87dfbc1a82d75f5
Takeru KIDO <keidy2463@gmail.com>
2025-01-26 18:47:26 +0900

    Execute ListUsers QuickSight API

--- main.py
+++ main.py
@@ -10,25 +10,51 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def register_quicksight_user(client: boto3.client, username: str) -> str:
-    """Register a QuickSight Reader User with QuickSight Identity Type.
+def get_non_registered_quicksight_users(
+    client: boto3.client, all_names: list[str]
+) -> list[str]:
+    """Get Non-registered QuickSight Users.
 
     Args:
         client (boto3.client): QuickSight Boto3 Client
-        username (str): QuickSight User Name
+        all_names (list[str]): All QuickSight User Names
+    Returns:
+        list[str]: Non-registered QuickSight Users
+    """
+
+    # Execute ListUsers API
+    response = client.list_users(
+        AwsAccountId=os.getenv("AWS_ACCOUNT_ID"),
+        Namespace="default",
+    )
+
+    # Filter Non-registered QuickSight Users
+    registered_names = [user["UserName"] for user in response["UserList"]]
+    return [name for name in all_names if name not in registered_names]
+
+
+def register_quicksight_user(client: boto3.client, name: str) -> str:
+    """Register a QuickSight Reader User whose Identity Type is QuickSight.
+
+    Args:
+        client (boto3.client): QuickSight Boto3 Client
+        name (str): QuickSight User Name
 
     Returns:
         str: Response value of UserInvitationUrl
     """
 
+    # Execute RegisterUser API
     response = client.register_user(
         AwsAccountId=os.getenv("AWS_ACCOUNT_ID"),
         Namespace="default",
         IdentityType="QUICKSIGHT",
         UserRole="READER",
         Email=os.getenv("EMAIL"),
-        UserName=username,
+        UserName=name,
     )
+
+    # Choose UserInvitationUrl from response
     return response["UserInvitationUrl"]
 
 
@@ -38,13 +64,18 @@ if __name__ == "__main__":
         "quicksight", region_name=os.getenv("AWS_REGION")
     )
 
-    # Read usernames from CSV file
+    # Read All Values of "name" Column from CSV file
     with open("usernames.csv", newline="", encoding="utf-8") as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            # Register a QuickSight user
-            user_invitation_url = register_quicksight_user(
-                quicksight_client, row["name"]
-            )
-            print(f"[register_quicksight_user] OK: {row['name']}")
-            print(f"UserInvitationUrl: {user_invitation_url}")  # DEBUG
+        usernames: list[str] = [row["name"] for row in csv.DictReader(file)]
+
+    # Get Non-registered QuickSight users
+    non_registered_usernames: list[str] = get_non_registered_quicksight_users(
+        quicksight_client, usernames
+    )
+    print(f"[get_non_registered_quicksight_users] OK: {len(non_registered_usernames)}")
+
+    for username in non_registered_usernames:
+        # Register a QuickSight user
+        user_invitation_url = register_quicksight_user(quicksight_client, username)
+        print(f"[register_quicksight_user] OK: {username}")
+        print(f"UserInvitationUrl: {user_invitation_url}")  # DEBUG
